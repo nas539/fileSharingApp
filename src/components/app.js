5c502f1be3bf3b05b8a156ce7efcd367985eace7
Nicholas Sharp <naas8905@gmail.com>
2020-06-30 08:49:26 -0700

    Adding input and click/change functions

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      file: {}
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDownload = this.handleDownload.bind(this);
+  }
+
+  handleDownload() {
+    const url = window.URL.createObjectURL(this.state.file);
+    const link = document.createElement("a");
+    link.download = this.state.file.name;
+    link.href = url;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(url);
+  }
+
+  handleChange(event) {
+    this.setState({
+      file: event.taget.files[0]
+    });
+  }
+
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <input onChange={this.handleChange} type="file"/>
+        <button onClick={this.handleDownload}>Download</button>
       </div>
     );
   }
